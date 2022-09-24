f0da02baa87bd1c3905e7994e928dcf5aef51bf8
amirali-ashraf <amiraliash.dev@gmail.com>
2022-09-23 23:55:23 -0400

    add validation & fix helper

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,21 @@
 class Character < ApplicationRecord
+  attribute :total_score
+  validates :name, uniqueness: true
+  validates :first_type, presence: true
+  validates :second_type, presence: true
+  validates :hp_score, numericality: true
+  validates :attack_score, numericality: true
+  validates :defense_score, numericality: true
+  validates :sp_attack_score, numericality: true
+  validates :sp_defense_score, numericality: true
+  validates :speed, numericality: true
+  validates :is_legendary, inclusion: [true, false]
+  validates :generation, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 6
+  }
+
   TYPES = [
     :bug,
     :dark,
@@ -24,6 +41,12 @@ class Character < ApplicationRecord
   enum :second_type, TYPES, prefix: :second_type
 
   def total_score
-    hp_score + attack_score + defense_score + sp_attack_score + sp_defense_score + speed
+    total_score = 0
+    total_score += hp_score if hp_score
+    total_score += attack_score if attack_score
+    total_score += defense_score if defense_score
+    total_score += sp_attack_score if sp_attack_score
+    total_score += sp_defense_score if sp_defense_score
+    total_score += speed if speed
   end
 end
